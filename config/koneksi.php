2f29d58939d81e63307d151ec5fd6f7cc0b61343
chondroseto <35015778+chondroseto@users.noreply.github.com>
2019-04-19 01:43:40 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+$server   = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "uts2"; 
+ 
+// Koneksi dan memilih database di server 
+mysql_connect($server,$username,$password) or die("Gagal"); 
+mysql_select_db($database) or die("Database tidak ditemukan"); 
+?> 
+ 
